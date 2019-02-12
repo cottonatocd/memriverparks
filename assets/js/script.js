a38b49ce0a6ad70571e29dc48fb0c42bbe9eab68
cottonatocd <cotton@ocdagency.com>
2019-02-12 16:50:27 -0500

    randomize hover color algorithm

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,19 +8,7 @@
 //     }
 // });
 
-
-
-
-
 $(document).ready(function(){
-
-
-    
-    // $('#myDropdown').on('show.bs.dropdown', function () {
-    //     // do something…
-    //     console.log("shown");
-    // });
-
 
     // cache the element
     var $navBar = $('.nav-wrapper');
@@ -29,8 +17,6 @@ $(document).ready(function(){
     var navPos = $navBar.outerHeight();
 
     $('.nav-placeholder').css('height', navPos);
-
-    console.log(navPos);
 
     // on scroll
     $(window).scroll(function() {
@@ -47,82 +33,23 @@ $(document).ready(function(){
 
     });
 
-
-
-
-    var palette = ['#44b549', '#ffc818', '#f05f22', '#298fce', '#e684a1'];
-
-    // $("a.btn").hover(function(){
-    //   $(this).css("background-color", "yellow");
-    //   }, function(){
-    //   $(this).css("background-color", "pink");
-    // });
-
-   
-    // colorCycle(palette);
-
-
-//     $("a.btn").mouseover(function(){
-//         palette = shuffle(palette);
-//         (function recurse(counter) {
-//             var color = palette[counter];
-//             $(this).css("background-color", color);
-//             console.log(color);
-//             delete palette[counter];
-//             palette.push(color);
-//             setTimeout(function() {
-//                 recurse(counter + 1);
-//             }, 300);
-//         })(0);
+    // var palette = ['#44b549', '#ffc818', '#f05f22', '#298fce', '#e684a1'];
     
-//     });
+    var animations = ['bgcolorchange1', 'bgcolorchange2', 'bgcolorchange3', 'bgcolorchange4'];
+
+    $("a.btn").mouseleave(function(){
+        var css = 'a.btn.btn-primary:hover, a.btn.btn-secondary:hover, .inverse a.btn.btn-secondary:hover, .dropdown-menu.show li a.btn:hover{ animation-name:' +  animations[Math.floor(Math.random()*animations.length)] +' }';
+        var style = document.createElement('style');
+    
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+    
+        document.getElementsByTagName('head')[0].appendChild(style);
+    });
+
+
 
  });
-
-
-// function shuffle(array) {
-//     var currentIndex = array.length, temporaryValue, randomIndex;
-  
-//     // While there remain elements to shuffle...
-//     while (0 !== currentIndex) {
-  
-//       // Pick a remaining element...
-//       randomIndex = Math.floor(Math.random() * currentIndex);
-//       currentIndex -= 1;
-  
-//       // And swap it with the current element.
-//       temporaryValue = array[currentIndex];
-//       array[currentIndex] = array[randomIndex];
-//       array[randomIndex] = temporaryValue;
-//     }
-  
-//     return array;
-// }
-
-// function colorCycle(array) {
-//     array = shuffle(array);
-//     for (var x = 0; x < array.length; x++) {
-//         setTimeout(function(y) {    
-//             console.log(array[y]);
-//         }, x * 500, x); // we're passing x
-//     }
-// }
-
-
-
-
-
-
-// var newsArray = [];   // your code puts strings into this array
-// var curNewsIndex = -1;
-
-// function advanceNewsItem() {
-//     ++curNewsIndex;
-//     if (curNewsIndex >= newsArray.length) {
-//         curNewsIndex = 0;
-//     }
-//     setTickerNews(newsArray[curNewsIndex]);   // set new news item into the ticker
-// }
-
-
-
