941d22613fdb3f7d0112e2b0798f906ba97b32f9
cottonatocd <cotton@ocdagency.com>
2019-02-21 12:30:07 -0500

    medium pulled

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,11 @@
 
 $(document).ready(function(){
 
+
+
+
+
+
     // cache the element
     var $navBar = $('.nav-placeholder .nav-wrapper');
 
@@ -61,29 +66,110 @@ $(document).ready(function(){
         $(p).find( '.plus' ).text("+");
     });
 
-    var getJSON = function(url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'json';
-        xhr.onload = function() {
-          var status = xhr.status;
-          if (status === 200) {
-            callback(null, xhr.response);
-          } else {
-            callback(status, xhr.response);
-          }
-        };
-        xhr.send();
-    };
+    // var getJSON = function(url, callback) {
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.open('GET', url, true);
+    //     xhr.responseType = 'json';
+    //     xhr.onload = function() {
+    //       var status = xhr.status;
+    //       if (status === 200) {
+    //         callback(null, xhr.response);
+    //       } else {
+    //         callback(status, xhr.response);
+    //       }
+    //     };
+    //     xhr.send();
+    // };
 
-    getJSON('https://cors.io/?https://medium.com/memriverparks/latest?format=json',
-    function(err, data) {
-    if (err !== null) {
-        console.log('Something went wrong: ' + err);
-    } else {
-        console.log(data);
-    }
-    });
+    // getJSON('https://cors.io/?https://medium.com/memriverparks/latest?format=json',
+    // function(err, data) {
+    // if (err !== null) {
+    //     console.log('Something went wrong: ' + err);
+    // } else {
+    //     console.log(JSON.parse(data));
+    // }
+    // });
+
+
+    // $.ajax({
+    //     type: "POST", //rest Type
+    //     dataType: 'jsonp', //mispelled
+    //     url: "http://cors.io/?https://medium.com/memriverparks/latest?format=json",
+    //     async: false,
+    //     contentType: "application/json; charset=utf-8",
+    //     success: function (msg) {
+    //         console.log(JSON.parse(msg));                
+    //     }
+    // });
+
+
+    fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/memriverparks')
+   .then((res) => res.json())
+   .then((data) => {
+      // Filter for acctual posts. Comments don't have categories, therefore can filter for items with categories bigger than 0
+      const res = data.items //This is an array with the content. No feed, no info about author etc..
+      const posts = res.filter(item => item.categories.length > 0) // That's the main trick* !
+
+      // Functions to create a short text out of whole blog's content
+      function toText(node) {
+         let tag = document.createElement('div')
+         tag.innerHTML = node
+         node = tag.innerText
+         return node
+      }
+      function shortenText(text,startingPoint ,maxLength) {
+         return text.length > maxLength?
+         text.slice(startingPoint, maxLength):
+         text
+      }
+
+      // Put things in right spots of markup
+      let output = '';
+      posts.forEach((item) => {
+        //  output += `
+        //  <li class="blog__post">
+        //     <a href="${item.link}">
+        //        <img src="${item.thumbnail}" class="blog__topImg"></img>
+        //        <div class="blog__content">
+        //           <div class="blog_preview">
+        //              <h2 class="blog__title">${shortenText(item.title, 0, 30)+ '...'}</h2>
+        //              <p class="blog__intro">${'...' + shortenText(toText(item.content),60, 300)+ '...'}</p>
+        //           </div>
+        //           <hr>
+        //           <div class="blog__info">
+        //              <span class="blog__author">${item.author}</span>
+        //              <span class="blog__date">${shortenText(item.pubDate,0 ,10)}</span>
+        //           </div>
+        //        </div>
+        //     <a/>
+        //  </li>`
+        output += `
+        <div class="row pt-4 pb-2">
+            <div class="col-5 offset-1">
+                <div class="row">
+                    <div class="col-10">
+                            <h3 class="mb-2">${shortenText(item.pubDate,0,10)}</h3>
+                            <h2>${item.title}</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <p class="mb-3">Foundation’s support to be celebrated this Friday at Grand Opening of the River Line and River Garden.</p>
+                        <a href="${item.link}" class="btn btn-secondary" target='_blank'>Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5">
+                <img class="img-fluid" src="${item.thumbnail}">
+            </div>
+        </div>
+        `
+        //console.log(new Date(item.pubDate));
+        console.log(item);
+      })
+      document.querySelector('#news-items').innerHTML = output
+    //console.log(output);
+})
     
 
  });
